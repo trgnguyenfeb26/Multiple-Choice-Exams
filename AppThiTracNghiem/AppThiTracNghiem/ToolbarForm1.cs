afab6701095fd747d17a8c017253418917dc12c5
Trung Nguyên <nguyennguyentrung01@gmail.com>
2022-05-10 21:09:58 +0700

    t

---
+++ AppThiTracNghiem/AppThiTracNghiem/ToolbarForm1.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraBars;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppThiTracNghiem
+{
+    public partial class ToolbarForm1 : DevExpress.XtraEditors.XtraForm
+    {
+        public ToolbarForm1()
+        {
+            InitializeComponent();
+        }
+    }
+}
