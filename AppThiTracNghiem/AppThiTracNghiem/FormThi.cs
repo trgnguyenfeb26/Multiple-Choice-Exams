af80306110d6488c15a257d9d65c371710cfc444
Trung Nguyên <nguyennguyentrung01@gmail.com>
2022-05-11 13:32:18 +0700

    create FormThi

--- AppThiTracNghiem/AppThiTracNghiem/FormThi.cs
+++ AppThiTracNghiem/AppThiTracNghiem/FormThi.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DevExpress.XtraBars;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,9 +11,9 @@ using System.Windows.Forms;
 
 namespace AppThiTracNghiem
 {
-    public partial class Form2 : Form
+    public partial class FormThi : DevExpress.XtraEditors.XtraForm
     {
-        public Form2()
+        public FormThi()
         {
             InitializeComponent();
         }
